4e0195da8e1507ba37cf7ce283a8663e64a87811
alexander <oulfc93@gmail.com>
2021-11-13 20:33:59 +0300

    routing login/home

--- src/index.js
+++ src/index.js
@@ -5,27 +5,18 @@ import 'regenerator-runtime/runtime.js';
 
 import '../assets/application.scss';
 
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Component from './components/App.jsx';
+
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const p = document.createElement('p');
-p.classList.add('card-text');
-p.textContent = 'It works!';
-
-const h5 = document.createElement('h5');
-h5.classList.add('card-title');
-h5.textContent = 'Project frontend l4 boilerplate';
-
-const cardBody = document.createElement('div');
-cardBody.classList.add('card-body');
-cardBody.append(h5, p);
-
-const card = document.createElement('div');
-card.classList.add('card', 'text-center');
-card.append(cardBody);
-
-const container = document.querySelector('#chat');
-container.append(card);
+ReactDOM.render(
+  <Component />,
+  document.querySelector('#chat'),
+);
 
 console.log('it works!');
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -40,6 +40,10 @@ module.exports = {
           { loader: 'sass-loader' },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
   },
 };
